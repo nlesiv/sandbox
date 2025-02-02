0c8fbe316afff9fdd493f2c5f5724a90caef9503
Nazar Lesiv <nazarlesiv@Mac.fios-router.home>
2025-02-01 20:56:56 -0500

    Update Flask image recognition example

--- Python3/Flask/HelloWorld/helloWorld.py
+++ Python3/Flask/HelloWorld/helloWorld.py
@@ -1,5 +1,60 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+
 app = Flask(__name__)
+api = Api(app)
+
+client = MongoClient("mongodb://sandbox:Fuckthisplace$01@mongo.statninja.net:27017")
+db = client.sandbox
+UserNum = db['UserNum']
+UserNum.insert_one({
+    'num_of_users': 0
+})
+
+class Visit(Resource):
+    def get(self):
+        prev_num = UserNum.find({})[0]['num_of_users']
+        new_num = prev_num+1;
+        UserNum.update_one({}, {"$set": {"num_of_users": new_num}})
+        return str("hello user" + str(new_num));
+
+
+# Rest Model with the basic Crud Operations
+class Add(Resource):
+    def post(self):
+        # fim ih ere, thene the resour add was requested using post
+        # Get posted data
+        postedData = request.get_json()
+        x = int(postedData['x'])
+        y =  int(postedData['y'])
+
+        respMap = {
+            'sum': x +y,
+            'status': 200
+        }
+        return jsonify(respMap)
+
+    def get(self):
+        return ""
+
+    def put(self):
+        return ""
+
+    def delete(self):
+        return ""
+
+
+class Subtract(Resource):
+    pass
+
+class Divide(Resource):
+    pass
+
+
+api.add_resource(Add, '/add')
+api.add_resource(Visit, '/visit')
+
 
 @app.route('/')
 def hello_world():
---
+++ Python3/Flask/ImageClassification/web/app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+import bcrypt
+import numpy as np
+import requests
+
+app = Flask(__name__)
+
+api = Api(app)
+
+# Init Mongo Client
+client = MongoClient("mongodb://sandbox:Fuckthisplace$01@mongo.statninja.net:27017")
+db = client.sandbox
+users = db['users']
+
+def user_exists(username):
+    if users.count_documents({"username": username}) == 0:
+        return False
+    else:
+        return True
+
+class Register(Resource):
+    def post(self):
+        # Get the post data
+        data = request.get_json()
+
+        # Get username and password
+        username = data['username']
+        password = data['password']
+
+        # check if user already exists
+        if user_exists(username):
+            return jsonify({
+                "status": 301,
+                'message': "Invalid username, user already exists"
+            })
+        # if user is new has passwword
+        hashed = bcrypt.hashpw(password.encone('utf8'), bcrypt.gensalt())
+        # store user in database
+        users.insert_one({
+            'username': username,
+            'password':hashed,
+            'token': 4
+        })
+        # return success
+        return jsonify({
+            "status":200,
+            "message": "Succesfully Registered"
+        })
+
+
+api.add_resource(Resource, '/register')
