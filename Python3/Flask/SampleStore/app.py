3683ac0cd80e8519a74453fbb9d710f1d2d5746b
Nazar Lesiv <nazarlesiv@MBP-14-2021.local>
2025-03-01 12:27:47 -0500

    Update flask env and compose files

---
+++ Python3/Flask/SampleStore/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+stores = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name": 'Chair',
+                "price": 16.99
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_stores():
+    return ({"stores": stores}, 200)
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return (new_store, 200)
+
+@app.post("/store/<string:name>/items")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return (new_item, 201)
+
+    return ({"message": "Store not found"}, 404)
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return (store, 200)
+
+    return ({"message": "Store not found"}, 404);
