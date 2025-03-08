e21de9e0706448a0a0b471860057fe6beb848e20
Nazar Lesiv <nazarlesiv@Mac.fios-router.home>
2025-03-08 18:10:08 -0500

    sample JWT APIs and migrations

--- Python3/Flask/SampleStore/schemas.py
+++ Python3/Flask/SampleStore/schemas.py
@@ -14,3 +14,8 @@ class ItemUpdateSchema(Schema):
 class StoreSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
