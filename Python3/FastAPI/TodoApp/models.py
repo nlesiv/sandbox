851e391e2cd900dd4b17bea55953e57711ead333
Nazar Lesiv <nazarlesiv@MBP-16-2023.local>
2025-05-11 13:10:44 -0400

    initial todo setup

---
+++ Python3/FastAPI/TodoApp/models.py
@@ -0,0 +1,14 @@
+import datetime
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from database import Base
+
+class Todos(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    completed = Column(Boolean, default=False)
+    # created_at = Column(DateTime, default=datetime.utcnow)
+    # updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
